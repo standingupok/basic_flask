3165ef3b36ceafd498f48126c94e15cfac77be0a
Hoang Long <nguyenhoanglong0165@gmail.com>
2024-10-26 21:09:29 +0700

    first page with POST method

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template, request
 app = Flask(__name__)
 
-@app.route('/index')
+@app.route('/')
 def hello_word():
-    return '<p>Long</p>'
+    return render_template('home.html')
 
-@app.route('/admin')
-def hello_admin():
-    return f"<h1>Hello admin </h1>"
+# @app.route('/')
+# def hello_word():
+#     return render_template('index.html', content='Long', hlong=['long1','long2','long3'])
+
+# @app.route('/admin')
+# def hello_admin():
+#     return f"<h1>Hello admin </h1>"
 
 @app.route('/user/<name>')
 def hello_user(name):
@@ -15,7 +19,13 @@ def hello_user(name):
         return redirect(url_for('hello_admin'))
     return f"<h1>Hello {name} </h1>"
 
-
+@app.route('/login', methods=['POST','GET'])
+def login():
+    if request.method == 'POST':
+        name = request.form['name']
+        if name: 
+            return redirect(url_for('hello_user', name=name))
+    return render_template('login.html')
 
 # @app.route('/blog/<int:blog_id>')
 # def blog(blog_id):
